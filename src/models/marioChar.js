91351318c7a54ef176c53aca415f6919af239685
mayank1503 <mayankmotwani1503@gmail.com>
2020-12-09 18:13:09 +0530

    Test

--- src/models/marioChar.js
+++ src/models/marioChar.js
@@ -1,6 +1,16 @@
-const mongoose = require('mongoose');
-
+const {Schema} = mongoose
 //  Your code goes here
+const marioSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    weight:{
+        type: Number,
+        required: true,
+    },
+    
+})
+const marioModel = mongoose.model('marioChar',marioSchema)
 
-
-module.exports = marioModel;
+module.exports = marioModel
